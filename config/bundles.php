412e81bb90eb4909785a75e3017075999ea6ce12
Damien Chauveau <damienchauveau@hotmail.fr>
2026-07-02 15:10:03 +0200

    style: cs-fixer

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
--- src/Form/LoginType.php
+++ src/Form/LoginType.php
@@ -50,7 +50,11 @@ class LoginType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([]);
+        $resolver->setDefaults([
+            'csrf_protection' => true,
+            'csrf_field_name' => '_csrf_token',
+            'csrf_token_id' => 'authenticate',
+        ]);
     }
 
     public function getBlockPrefix(): string
