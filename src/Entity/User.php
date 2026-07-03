2ed3eb21f0336839ae0745b7193934cbec1b553c
Damien Chauveau <damienchauveau@hotmail.fr>
2026-07-03 09:53:14 +0200

    feat(security): login throttling and role model, phpstan fixes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -87,7 +87,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, \String
     public function getRoles(): array
     {
         $roles = $this->roles;
-        $roles[] = 'ROLE_ADMIN';
+        $roles[] = 'ROLE_USER';
 
         return array_values(array_unique($roles));
     }
@@ -140,7 +140,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface, \String
 
     public function getFullName(): string
     {
-        return $this->firstName.' '.$this->lastName;
+        return $this->firstName . ' ' . $this->lastName;
     }
 
     public function getCreatedAt(): ?\DateTimeImmutable
